b214b4cdcc54691bb230b2da4f9c9923ed5f381f
szykor18 <szymonkornik.bs@gmail.com>
2024-03-12 22:30:14 +0100

    refactor

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from '../../environment';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class AuthService {
 
   private apiUrl = environment.backendApi;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId : Object) { }
   
   login(username: string, password: string): Observable<boolean> {
     return this.http.post<any>(this.apiUrl + '/token', { username, password }).pipe(
@@ -30,7 +31,11 @@ export class AuthService {
   }
 
   isLoggedIn(): boolean {
-    return !!localStorage.getItem('currentUserToken');
+    if (isPlatformBrowser(this.platformId)) {
+      const token = localStorage.getItem('currentUserToken');
+      return token != null;
+    }
+    return false;
   }
 
   register(user: { username: string, password: string }): Observable<any> {
