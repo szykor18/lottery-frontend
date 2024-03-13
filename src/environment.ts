86ea3b740b2831bb2f43ecca8f77e44ee921b7a2
szykor18 <szymonkornik.bs@gmail.com>
2024-03-13 23:03:20 +0100

    changed environment

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    backendApi : 'http://ec2-3-126-250-7.eu-central-1.compute.amazonaws.com:8000'
-    // backendApi : 'http://localhost:8080'
+    // backendApi : 'http://ec2-3-126-250-7.eu-central-1.compute.amazonaws.com:8000'
+    backendApi : 'http://localhost:8000'
 };
