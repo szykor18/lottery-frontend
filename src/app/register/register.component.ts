7d9c86a3e350e9024166d6282639eb3648127a78
Szymon Kornik <szymon.kornik@corvus-it.com>
2024-06-08 14:37:31 +0200

    refactor

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import { AuthService } from '../service/auth.service';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
   username: string = '';
   password: string = '';
+  errorMessage: string = '';
 
   constructor(private authService: AuthService, private router: Router) { }
 
@@ -23,6 +25,7 @@ export class RegisterComponent {
       },
       error => {
         console.error('Registration failed', error);
+        this.errorMessage = error.error.message;
       }
     );
   }
--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    // backendApi : 'http://ec2-3-126-250-7.eu-central-1.compute.amazonaws.com:8000'
-    backendApi : 'http://localhost:8000'
+    backendApi : 'http://ec2-18-193-71-195.eu-central-1.compute.amazonaws.com:8000'
+    // backendApi : 'http://localhost:8000'
 };
